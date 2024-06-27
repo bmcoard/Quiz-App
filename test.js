1efc077c4dc0e2c3c09bd078eb36b6808e4d15ac
bmcoard <bmcoard@gmail.com>
2024-06-27 18:30:51 -0400

    add API support

--- test.js
+++ test.js
@@ -1,5 +1,23 @@
+let q1 = {
+    question: "What is 1 foot in inches?",
+    optA: "1 inch",
+    optB: "2 inches",
+    optC: "3 inches",
+    optD: "12 inches",
+    correct: "D"
+}
 
-let quizQs = []
+let q2 = {
+    question: "What is the color of the sun?",
+    optA: "yellow",
+    optB: "purple",
+    optC: "black",
+    optD: "blue",
+    correct: "A"
+}
+
+
+let quizQs = [q1, q2]
 let currentQ_index = 0
 let score = 0
 
@@ -15,39 +33,41 @@ let alreadySelected = false
 Array.from(options).forEach((option) => option.addEventListener("click", handleClick))
 
 getQuestions()
+//loadQuestion()
 document.getElementById("next").addEventListener("click", (event) => changeQuestion(1))
 document.getElementById("previous").addEventListener("click", (event) => changeQuestion(-1))
 document.getElementById("submit").addEventListener("click", (event) => submitQuiz())
 
+//console.log(quizQs)
 async function getQuestions() {
-    let response = await fetch( "https://opentdb.com/api.php?amount=10" );
-    if( !response.ok ) {
-        console.error( "Failed to retrieve Quiz Questions from API" );
-        return;
-    }
+    // let response = await fetch( "https://opentdb.com/api.php?amount=10" );
+    // if( !response.ok ) {
+    //     console.error( "Failed to retrieve Quiz Questions from API" );
+    //     return;
+    // }
 
-    let data = await response.json();
-    let raw = data.results;
+    // let data = await response.json();
+    // let raw = data.results;
 
     let apiKey = "3aGi3iTtljVSOSWg6x77gN7M7gQK3KKRWqwT79un"
-    let r = await fetch(`https://quizapi.io/api/v1/questions?apiKey=${apiKey}&limit=10`)
+    let r = await fetch(`https://quizapi.io/api/v1/questions?apiKey=${apiKey}&category=Code`)
     let d = await r.json()
-    //let raw =
 
-
+    
     console.log("NEW DATA:", d)
-    quizQs = processQuestions(raw)
+    quizQs = processQuestions(d)
+    console.log("quizQ=", quizQs)
     loadQuestion()
 }
 
 function loadQuestion(){
     currentQ = quizQs[currentQ_index]
-    
+
     document.getElementById("title").textContent = currentQ.question
-    getA.textContent = currentQ.optionA
-    getB.textContent = currentQ.optionB
-    getC.textContent = currentQ.optionC
-    getD.textContent = currentQ.optionD
+    getA.textContent = currentQ.optA
+    getB.textContent = currentQ.optB
+    getC.textContent = currentQ.optC
+    getD.textContent = currentQ.optD
 
     if(isSubmitted){
         gradeAnswer()
@@ -55,23 +75,36 @@ function loadQuestion(){
 
 }
 
-function processQuestions(raw){
-    return raw.map(question => {
-        //question = question.question; redundant?
-        const optA = question.incorrect_answers[0];
-        const optB = question.incorrect_answers[1];
-        const optC = question.incorrect_answers[2];
-        const optD = question.correct_answer;
-        const correct = "D";
-        return {
+function processQuestions(data){
+    console.log(data)
+    results= data.map(question => {
+        const optA = question.answers.answer_a;
+        const optB = question.answers.answer_b;
+        const optC = question.answers.answer_c;
+        const optD = question.answers.answer_d;
+        let correct = null
+
+        const myObject = question.correct_answers
+        for(key in myObject){
+            if(myObject[key]=="true"){
+                let keyFormatted = key.split("_")[1].toUpperCase()
+                correct = keyFormatted
+                break
+            }
+        }
+        let myQuestion = {
             question: question.question,
-            optionA: optA,
-            optiontB: optB,
-            optionC: optC,
-            optionD: optD,
+            optA: optA,
+            optB: optB,
+            optC: optC,
+            optD: optD,
             correct
         };
-    })  
+        console.log(myQuestion)
+        return myQuestion
+    }) 
+    console.log("result=", results) 
+    return results
 }
 
 function changeQuestion(direction){
@@ -140,7 +173,7 @@ function resetQuestion(direction){
 
     let grade = document.getElementById("grade")
 
-    console.log(prevSelectedForLastQuestion)
+    console.log("null:", prevSelectedForLastQuestion)
 
     if(prevSelectedForLastQuestion != null){
         prevSelectedForLastQuestion.classList.remove("selected")
